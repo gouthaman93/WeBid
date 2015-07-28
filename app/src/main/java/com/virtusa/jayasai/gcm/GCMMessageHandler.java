1cc2f3d42143ff9e79a99e63c94e4fb49fc423d9
Jayasai Goutheman <gouthaman93@gmail.com>
2015-07-28 12:12:04 +0530

    added GCM registering and reciver process (Notification modification pending)

---
+++ app/src/main/java/com/virtusa/jayasai/gcm/GCMMessageHandler.java
@@ -0,0 +1,35 @@
+package com.virtusa.jayasai.gcm;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+
+/**
+ * Created by jayasaigoutheman on 7/28/15.
+ */
+public class GCMMessageHandler extends GcmListenerService{
+
+    public static final int MESSAGE_NOTIFICATION_ID = 435345;
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+
+        createNotification(from, message);
+    }
+
+    // Creates notification based on title and body received
+    private void createNotification(String title, String body) {
+        Context context = getBaseContext();
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(android.R.mipmap.sym_def_app_icon).setContentTitle(title)
+                .setContentText(body);
+        NotificationManager mNotificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(MESSAGE_NOTIFICATION_ID, mBuilder.build());
+    }
+}
--- app/src/main/java/com/virtusa/jayasai/webid/Home_Activity.java
+++ app/src/main/java/com/virtusa/jayasai/webid/Home_Activity.java
@@ -7,11 +7,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GcmReceiver;
+import com.virtusa.jayasai.gcm.*;
 
 
 public class Home_Activity extends ActionBarActivity {
 
     WebView mainWebview;
+    private GCMClientManager pushClientManager;
+    String PROJECT_NUMBER = "193167212796";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,30 @@ public class Home_Activity extends ActionBarActivity {
 
         mainWebview.loadUrl("https://www.google.lk");
 
+        //Register for push notification
+        pushClientManager = new GCMClientManager(this, PROJECT_NUMBER);
+        pushClientManager.registerIfNeeded(new GCMClientManager.RegistrationCompletedHandler() {
+            @Override
+            public void onSuccess(String registrationId, boolean isNewRegistration) {
+                Toast.makeText(getApplicationContext(), registrationId,
+                        Toast.LENGTH_SHORT).show();
+                // SEND async device registration to your back-end server
+                // linking user with device registration id
+                // POST https://my-back-end.com/devices/register?user_id=123&device_id=abc
+            }
 
+            @Override
+            public void onFailure(String ex) {
+                super.onFailure(ex);
+
+                Toast.makeText(getApplicationContext(), "Error Occured while registering the Device",
+                        Toast.LENGTH_SHORT).show();
+
+                // If there is an error registering, don't just keep trying to register.
+                // Require the user to click a button again, or perform
+                // exponential back-off when retrying.
+            }
+        });
 
     }
 
